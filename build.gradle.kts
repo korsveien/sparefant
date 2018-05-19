cba0d3c86ce1f101a6bf5ee1615b87d9f16fede2
Peder Korsveien <peder.korsveien@nav.no>
2018-05-19 07:07:18 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+repositories {
+    jcenter()
+}
+
+plugins {
+    application
+    kotlin("jvm") version "1.2.31"
+    id ("com.github.johnrengelman.shadow") version "2.0.4"
+}
+
+tasks {
+    "stage" {
+        dependsOn("clean", "shadowJar")
+        mustRunAfter("build")
+    }
+}
+
+application {
+    mainClassName = "no.sparefant.MainKt"
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    compile("io.javalin:javalin:1.6.1")
+    compile("com.github.kittinunf.fuel:fuel:1.13.0")
+    compile ("io.github.microutils:kotlin-logging:1.5.4")
+    compile ("ch.qos.logback:logback-classic:1.3.0-alpha4")
+}
+
+
---
+++ src/main/kotlin/no/sparefant/Main.kt
@@ -0,0 +1,29 @@
+package no.sparefant
+
+import com.github.kittinunf.fuel.httpPost
+import io.javalin.Javalin
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+//    fetchToken()
+    val port = System.getenv("PORT")?.toInt() ?: 3000
+    logger.info { "PORT is $port" }
+    val app = Javalin.start(port)
+    app.get("/") { it.result("Hello") }
+}
+
+private fun fetchToken() {
+    val clientID = System.getenv("SBANKEN_CLIENT_ID")
+    val secret = System.getenv("SBANKEN_SECRET")
+
+    val (_, response, _) = "https://api.sbanken.no/identityserver/connect/token"
+            .httpPost()
+            .header("Content-Type" to "application/x-www-form-urlencoded")
+            .authenticate(clientID, secret)
+            .body("grant_type=client_credentials", Charsets.UTF_8)
+            .responseString()
+
+    println(response)
+}
